c54acb5ee2adadddd3d4769bcdbf699cbf9dca74
Try Indra Hatmojo <tryindra3375@gmail.com>
2025-04-11 18:31:15 +0700

    api cuaca

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("hello world 2 222 branch indra")
+let x = ["s","x"]
+let y = x.slice()
+console.log(y)
+
+async function getWheater(city){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ab1d0e94d05ee646ef7d882325d5a867`)
+    let data = await response.json()
+    console.log(data)
+}
+
+getWheater("surabaya")
