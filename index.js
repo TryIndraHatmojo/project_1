353e861314c654114b9fe4f242778f22dc9dc9f5
Try Indra Hatmojo <tryindra3375@gmail.com>
2025-04-12 03:22:22 +0700

    hilangin api key

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ let x = ["s","x"]
 let y = x.slice()
 console.log(y)
 
-async function getWheater(city){
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ab1d0e94d05ee646ef7d882325d5a867`)
-    let data = await response.json()
-    console.log(data.weather[0].main+", "+data.weather[0].description)
+async function getApi(){
+    try{
+        const getApi = await fetch(`./config.json`)
+        const api = await getApi.json()
+        return api
+    }catch(error){
+        console.log(error.message)
+    }
 }
 
-getWheater("sorong")
+async function getWheater(city){
+    try{
+        const getApi = await getApi();
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${getApi}`)
+        let data = await response.json()
+        console.log(data.weather[0].main+", "+data.weather[0].description)
+    }catch(error){
+        console.log(error.message)
+    }
+    
+}
+
+
+getApi()
+// getWheater("sorong")
