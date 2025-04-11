0cae339fcea90eecd9750774f5788bb7722868f5
Try Indra Hatmojo <tryindra3375@gmail.com>
2025-04-11 18:41:24 +0700

    menampilkan cuaca surabaya

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(y)
 async function getWheater(city){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ab1d0e94d05ee646ef7d882325d5a867`)
     let data = await response.json()
-    console.log(data)
+    console.log(data.weather[0].main+", "+data.weather[0].description)
 }
 
 getWheater("surabaya")
